cfef41e26987311d884176d164c05ac3531097d0
Rishita Galande <galanderishita@gmail.com>
2026-03-01 17:00:33 +0530

    Used env backend url in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:5000";
+export const server = import.meta.env.VITE_BACKEND_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
