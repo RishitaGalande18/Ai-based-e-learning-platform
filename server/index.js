2080c7920b3a99c89ce4faad37dfc8f1cc214f20
Rishita Galande <galanderishita@gmail.com>
2026-03-01 14:21:03 +0530

    updated index.js for production

--- server/index.js
+++ server/index.js
@@ -1,24 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
-// import Razorpay from "razorpay";
 import cors from "cors";
-
 
 dotenv.config();
 
-// export const instance = new Razorpay({
-//   key_id: process.env.Razorpay_Key,
-//   key_secret: process.env.Razorpay_Secret,
-// });
-
 const app = express();
 
-// using middlewares
 app.use(express.json());
 app.use(cors());
-
-const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Server is working");
@@ -26,21 +16,20 @@ app.get("/", (req, res) => {
 
 app.use("/uploads", express.static("uploads"));
 
-// importing routes
 import userRoutes from "./routes/user.js";
 import courseRoutes from "./routes/course.js";
 import adminRoutes from "./routes/admin.js";
 
-// using routes
 app.use("/api", userRoutes);
 app.use("/api", courseRoutes);
 app.use("/api", adminRoutes);
 
+const port = process.env.PORT || 5000;
 
-
-
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-  connectDb();
+connectDb().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch((err) => {
+  console.log("Database connection failed:", err);
 });
